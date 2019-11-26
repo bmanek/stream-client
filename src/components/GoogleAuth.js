7d1a1ab286cbea729faf60cfe85503d58ebcfccc
bmanek <41875460+bmanek@users.noreply.github.com>
2019-11-25 20:45:13 -0500

    add .then action to componentDidMount, add renderAuthButton()

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -3,17 +3,33 @@ import React from 'react'
 const API_KEY = process.env.REACT_APP_MY_API_KEY
 
 class GoogleAuth extends React.Component {
+  state = { isSignedIn: null }
+
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
       window.gapi.client.init({
         clientId: API_KEY,
         scope: 'email',
+      }).then( () => {
+        this.auth = window.gapi.auth2.getAuthInstance()
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() })
       })
     })
   }
 
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>I don't know if we're signed in</div>
+    } else if (this.state.isSignedIn) {
+      return <div>You're signed in!</div>
+    } else {
+      return <div>You're not signed in :(</div>
+
+    }
+  }
+
   render() {
-    return <div>Google Auth</div>
+    return <div>{this.renderAuthButton()}</div>
   }
 }
 
