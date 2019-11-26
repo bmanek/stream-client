d7e618c610f9e9f8de4fcf6bb62fcef5c517decc
bmanek <41875460+bmanek@users.noreply.github.com>
2019-11-25 20:09:37 -0500

    add/obfuscate api key, add to componentDidMount

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
+const API_KEY = process.env.REACT_APP_MY_API_KEY
+
 class GoogleAuth extends React.Component {
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
       window.gapi.client.init({
-        clientId: "sample",
-        scope: 'email'
+        clientId: API_KEY,
+        scope: 'email',
       })
     })
   }
